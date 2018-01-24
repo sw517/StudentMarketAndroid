ee50cd108d186d0c3a91dd65246989f8021de919
Sam Wood <sw517@kent.ac.uk>
2018-01-24 19:55:16 +0000

    Initial commit

---
+++ app/src/main/java/dev/studentmarket/studentmarket/MainActivity.java
@@ -0,0 +1,26 @@
+package dev.studentmarket.studentmarket;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button registerUser = (Button) findViewById(R.id.bRegisterForm);
+        registerUser.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intentRegister = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
+    }
+}
