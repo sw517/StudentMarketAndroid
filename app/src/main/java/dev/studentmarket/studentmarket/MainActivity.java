d552b6a57c8e0545436e5d10835ca3b34ba48025
Deniz <da332@kent.ac.uk>
2018-02-07 14:44:29 +0000

    Began work on login attempts

--- app/src/main/java/dev/studentmarket/studentmarket/MainActivity.java
+++ app/src/main/java/dev/studentmarket/studentmarket/MainActivity.java
@@ -3,8 +3,10 @@ package dev.studentmarket.studentmarket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentRegister);
             }
         });
+    }
+
+    /** Called when the user presses login */
+    public void attemptLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        Log.d("MyApp","I am here");
+
+
+        startActivity(intent);
     }
 }
