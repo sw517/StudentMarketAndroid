597e8355f9a9926bcd4dd51a7354f30c9dfcee99
Deniz <da332@kent.ac.uk>
2018-02-09 16:53:14 +0000

    Testing the login POST request

--- app/src/main/java/dev/studentmarket/studentmarket/MainActivity.java
+++ app/src/main/java/dev/studentmarket/studentmarket/MainActivity.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -31,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
 
         Log.d("MyApp","I am here");
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://3cda1491.ngrok.io/api/login?email=da332@kent.ac.uk&password=deniz123";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("MyApp","Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MyApp","Error with request!"+ error.toString());
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
         startActivity(intent);
