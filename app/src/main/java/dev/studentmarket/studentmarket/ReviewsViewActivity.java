2f7a8a859f6d5c5c0f9196c63fa69685489b1541
Sam Wood <sw517@kent.ac.uk>
2018-03-08 11:17:09 +0000

    Started View-Reviews activity

--- app/src/main/java/dev/studentmarket/studentmarket/ReviewsViewActivity.java
+++ app/src/main/java/dev/studentmarket/studentmarket/ReviewsViewActivity.java
@@ -2,6 +2,7 @@ package dev.studentmarket.studentmarket;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Rating;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,10 +10,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -38,27 +43,25 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class ProfileActivity extends AppCompatActivity {
-
+public class ReviewsViewActivity extends AppCompatActivity {
     private Map<String, String> parameters = new HashMap<>(); // Here we store all of our parameters that are used in API requests
 
-    private String apiToken = "";
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Context className;
-    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_reviews_view);
+        String apiToken = ""; // CREATE INSTANCE TO ASSIGN FROM FILE
 
+        // NEEDED FOR NAVIGATION MENU
         className =  this.getApplicationContext();
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
-
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
@@ -81,39 +84,35 @@ public class ProfileActivity extends AppCompatActivity {
                             Intent intent = new Intent(className, MainActivity.class);
                             startActivity(intent);
                         }
+
                         return true;
                     }
                 });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // OPEN FILE TO GET LOCALLY STORED API TOKEN
-        apiToken = getAPIToken();
-
-        // OPEN FILE TO GET LOCALLY STORED API TOKEN
-        userId = getLocalUserId();
-
-        Intent intent = getIntent();
-        if(intent.hasExtra("userId")){
-            userId = getIntent().getExtras().getString("userId", "0");
-            Log.d("Found Extra", userId);
-        }
-
-        // SET PROFILE IMAGE IN NAV DRAWER
+        // SET PROFILE IMAGE AND NAME IN NAV DRAWER
         View hView =  navigationView.getHeaderView(0);
         ImageView navheaderimage = (ImageView) hView.findViewById(R.id.navheaderimage);
         TextView navheadername = (TextView) hView.findViewById(R.id.nav_header_name);
         navheadername.setText(getUserName());
 
         String imgURL = getProfileImg();
-        if (imgURL != null) {
+        Log.d("IMGURL", imgURL);
+        if (!imgURL.equals("null")) {
             String absoluteURL = "https://student-market.co.uk/storage/" + imgURL;
             Picasso.with(getApplicationContext()).load(absoluteURL).into(navheaderimage);
         }
 
+        // OPEN FILE TO GET LOCALLY STORED API TOKEN
+        apiToken = getAPIToken();
 
-        getRequest("https://student-market.co.uk/api/view/" + userId + "?api_token=" + apiToken, "view");
+        String userId = getIntent().getExtras().getString("userId", "0");
+        getReviews("https://student-market.co.uk/api/view/" + userId + "?api_token=" + apiToken, "view");
     }
 
+    /**
+     * Allows navigation button to be pressed
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -139,31 +138,6 @@ public class ProfileActivity extends AppCompatActivity {
             StringBuffer stringBuffer = new StringBuffer();
             while((fileString=bufferedReader.readLine()) != null) {
                 stringBuffer.append(fileString);
-                Log.d("APITOKENREADING", stringBuffer.toString());
-                String apiToken = stringBuffer.toString();
-                return apiToken;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Find local file containing API TOKEN
-     */
-    public String getLocalUserId() {
-        try {
-            String fileString;
-            FileInputStream fileInputStream = openFileInput("localUserId");
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
-            while((fileString=bufferedReader.readLine()) != null) {
-                stringBuffer.append(fileString);
-                Log.d("LocalUserId", stringBuffer.toString());
                 String apiToken = stringBuffer.toString();
                 return apiToken;
             }
@@ -222,9 +196,9 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     /**
-     * Submits a GET request to the API to get user details
+     * Submits a POST request to the API
      */
-    public void getRequest(String url, final String type) {
+    public void getReviews(String url, final String type) {
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -235,9 +209,9 @@ public class ProfileActivity extends AppCompatActivity {
 
                         try {
                             JSONObject json_response = new JSONObject(response);
-
-                            processData(json_response.getBoolean("success"), json_response.getString("message"), json_response.getJSONObject("data"));
-
+                            if (type.equals("items")) {
+                                processData(json_response.getBoolean("success"), json_response.getString("message"), json_response.getJSONObject("data"));
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -263,54 +237,97 @@ public class ProfileActivity extends AppCompatActivity {
      * Here we process the data our API provides us with
      */
     public void processData(Boolean success, String message, JSONObject data) {
+        ListView listView;
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> descriptions = new ArrayList<>();
+        ArrayList<String> userIds = new ArrayList<>();
+        ArrayList<Integer> ratings = new ArrayList<>();
 
         Log.d("Data", data.toString());
         Log.d("Success", success + "!");
         Log.d("Message", message);
 
-//      GET USER DATA
+        // GET REVIEW DATA
         try {
-            JSONObject userData = data.getJSONObject("viewUser");
-
-            // GET NAME
-            String firstNameStr = userData.getString("first_name");
-            String lastNameStr = userData.getString("last_name");
-            TextView nameET = (TextView) findViewById(R.id.profileName);
-            nameET.setText(firstNameStr + " " + lastNameStr);
-
             // GET REVIEWS
             JSONObject userReviews = data.getJSONObject("userReviews");
-            ArrayList<Integer> ratings = new ArrayList<>();
+            Log.d("Reviews", userReviews.toString());
             Iterator<?> keys = userReviews.keys();
             while( keys.hasNext() ) {
                 String key = (String)keys.next();
                 if ( userReviews.get(key) instanceof JSONObject ) {
+                    // ID
+                    String buyerId = userReviews.getJSONObject(key).getString("buyer_id");
+                    userIds.add(buyerId);
+                    // DESCRIPTION
+                    String description = userReviews.getJSONObject(key).getString("review");
+                    descriptions.add(buyerId);
+                    // RATING
                     int rating = userReviews.getJSONObject(key).getInt("rating");
                     ratings.add(rating);
                 }
             }
-            int averageRating = 0;
-            int totalRatings = ratings.size();
-            RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-            TextView numRatings = (TextView) findViewById(R.id.numReviews);
-            for (int i = 0; i < ratings.size(); i++) {
-                averageRating += ratings.get(i);
-            }
-            averageRating = (averageRating / totalRatings);
-            ratingBar.setRating(averageRating);
-            numRatings.setText("(" + Integer.toString(totalRatings) + ")");
+
+            // ADD ITEMS TO LIST VIEW ON SCREEN
+            listView = findViewById(R.id.listview);
+            ReviewAdapter adapter = new ReviewAdapter(this, names, descriptions, userIds, ratings);
+            listView.setAdapter(adapter);
+
+//            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+//                    Intent details = new Intent(ItemOverviewActivity.this, ItemDetailsActivity.class);
+//                    String itemId = ((TextView) view.findViewById(R.id.textviewid)).getText().toString();
+//                    String userId = ((TextView) view.findViewById(R.id.textviewuserid)).getText().toString();
+//                    details.putExtra("id", itemId);
+//                    details.putExtra("userId", userId);
+//                    startActivity(details);
+//
+//                }
+//            });
+
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
+}
 
-    /**
-     *  View Reviews of seller
-     */
-    public void viewReviews(View view) {
-        Intent details = new Intent(ProfileActivity.this, ReviewsViewActivity.class);
-        details.putExtra("userId", userId);
-        startActivity(details);
+/**
+ * This class goes through the view rows for the adapter and applies the data to the XML Elements
+ * Essentially this populates each list item
+ */
+class ReviewAdapter extends ArrayAdapter<String> {
+    Context context;
+    ArrayList<String> titleArray;
+    ArrayList<String> descriptionArray;
+    ArrayList<String> userIdArray;
+    ArrayList<Integer> ratingArray;
+
+    ReviewAdapter(Context c, ArrayList<String> titles, ArrayList<String> descriptions, ArrayList<String> userId, ArrayList<Integer> ratings) {
+
+        super(c, R.layout.single_row, R.id.textviewtitle, titles);
+        this.context = c;
+        this.titleArray = titles;
+        this.descriptionArray = descriptions;
+        this.userIdArray = userId;
+        this.ratingArray = ratings;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = layoutInflater.inflate(R.layout.single_row, parent, false);
+        TextView reviewName = row.findViewById(R.id.reviewUserName);
+        TextView reviewDescription = row.findViewById(R.id.reviewUserDescription);
+        RatingBar ratingBar = row.findViewById(R.id.ratingBar);
+//        TextView userId = row.findViewById(R.id.textviewuserid);
+
+        reviewName.setText(titleArray.get(position));
+        reviewDescription.setText(descriptionArray.get(position));
+        ratingBar.setRating(ratingArray.get(position));
+//        userId.setText(userIdArray.get(position));
+
+        return row;
     }
 }
